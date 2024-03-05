1cb4a714a83d332454bb8525614044e779d7c650
Federico De Luca <federicondeluca@gmail.com>
2024-03-04 23:33:47 -0300

    Initial Commit

---
+++ ScottPilgrimQuiz/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ScottPilgrimQuiz
+//
+//  Created by Federico De Luca on 4/3/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ScottPilgrimQuiz/ScottPilgrimQuizApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScottPilgrimQuizApp.swift
+//  ScottPilgrimQuiz
+//
+//  Created by Federico De Luca on 4/3/24.
+//
+
+import SwiftUI
+
+@main
+struct ScottPilgrimQuizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
