65f66a11e97cbad44bc59ff5e1395195469029e4
Federico De Luca <federicoNdeluca@gmail.com>
2024-03-06 23:35:06 -0300

    Layout multiplier added

---
+++ ScottPilgrimQuiz/Resources/LayoutMultiplier.swift
@@ -0,0 +1,16 @@
+//
+//  LayoutMultiplier.swift
+//  ScottPilgrimQuiz
+//
+//  Created by Federico De Luca on 6/3/24.
+//
+
+import Foundation
+
+struct LayoutMultiplier {
+    static func size(_ multiplier: CGFloat) -> CGFloat {
+        8 * multiplier
+    }
+    
+    private init() {}
+}
