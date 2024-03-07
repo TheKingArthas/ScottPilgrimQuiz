3946bb4850c4ddaabb254d641a11778851d7907c
Federico De Luca <federicoNdeluca@gmail.com>
2024-03-06 23:30:01 -0300

    Custom colors added

---
+++ ScottPilgrimQuiz/Resources/CustomColor.swift
@@ -0,0 +1,18 @@
+//
+//  CustomColor.swift
+//  ScottPilgrimQuiz
+//
+//  Created by Federico De Luca on 6/3/24.
+//
+
+import Foundation
+import SwiftUI
+
+struct CustomColor {
+    static var background: Color { Color("Background") }
+    static var primary: Color { Color("Primary") }
+    static var secondary: Color { Color("Secondary") }
+    static var white: Color { .white }
+    
+    private init() {}
+}
