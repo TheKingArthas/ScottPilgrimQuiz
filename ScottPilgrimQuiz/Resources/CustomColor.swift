d82917e3d7fa66daed2ee122122e36fd849fb9ef
Federico De Luca <federicondeluca@gmail.com>
2024-09-13 22:09:37 -0300

    Colors renaming

--- ScottPilgrimQuiz/Resources/CustomColor.swift
+++ ScottPilgrimQuiz/Resources/CustomColor.swift
@@ -9,11 +9,11 @@ import Foundation
 import SwiftUI
 
 struct CustomColor {
-    static var background: Color { Color("Background") }
-    static var destructive: Color { Color("Destructive") }
-    static var positive: Color { Color("Positive") }
-    static var primary: Color { Color("Primary") }
-    static var secondary: Color { Color("Secondary") }
+    static var background: Color { Color("BackgroundColor") }
+    static var destructive: Color { Color("DestructiveColor") }
+    static var positive: Color { Color("PositiveColor") }
+    static var primary: Color { Color("PrimaryColor") }
+    static var secondary: Color { Color("SecondaryColor") }
     static var white: Color { .white }
     
     private init() {}
